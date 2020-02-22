67a602648831355457bd32e22d16a77cdd650ae5
Amer-bit <https://github.com/Amer-bit>
2020-02-22 07:54:36 +0200

    added summation function

--- app.js
+++ app.js
@@ -8,16 +8,17 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
-    var theSum = a+b;
-    // 'The sum of 4 and 7 is 11.'
-    var message = `The sum of ${a} and ${b} is ${theSum}.`;
-    return [theSum, message];
+function sum(nosum1,nosum2) { //eslint-disable-line
+    var summation = nosum1 + nosum2;
+
+    var nofication = `The sum of ${nosum1} and ${nosum2} is ${summation}.`;
+
+    return [summation, nofication];
 }
-testSum(4,7);
+console.log(sum(4, 7));
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+ testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -120,7 +121,7 @@ This function should be dynamic, accepting an array of any length.
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyAnyArray() function and see if the test passes.*/
 
 // Write your code here
-var testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
+var testDynamicArray = [1, 2, 3, 4, 5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
 
